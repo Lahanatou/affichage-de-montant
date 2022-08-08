8c328012b1bb0b59a704cdbe35ee63f0c8e1c664
lahanatou <nouryous30@gmail.com>
2022-08-08 22:27:41 +0100

    first commit

---
+++ javascripts/sale.js
@@ -0,0 +1,11 @@
+function calc() {
+    console.log(document.getElementById("product"));
+  }
+  function calc() {
+    const price = document.getElementById("product").value;
+    const number = document.getElementById("number").value;
+    window.alert(`${price}The circle is.${number}Pieces. Subtotal.${price + number}Circle.`);
+    // Add the following for confirmation
+    console.log(typeof(price));
+    console.log(typeof(1));
+  }
---
+++ javascripts/sample.js
@@ -0,0 +1,57 @@
+
+<script type="text/javascript" src="javascripts/sample.js">...</script>
+
+const priceElement = document.getElementById("product");
+const numberElement = document.getElementById("number");
+
+let purchases = [];
+function add() {
+  const price = priceElement.value;
+  const number = numberElement.value;
+  
+  let purchase = {
+    price: parseInt(price),
+    number: parseInt(number),
+  };
+
+  const newPurchase = purchases.findIndex((item) => item.price === purchase.price)
+  if(purchases.length < 1 || newPurchase === -1) {
+    purchases.push(purchase)
+  } else {
+    purchases[newPurchase].number += purchase.number
+  }
+
+  window.alert(`${display()}\n小計${subtotal()}円`);
+  priceElement.value = "";
+  numberElement.value = "";
+}
+function subtotal() {
+  return purchases.reduce((prev, purchase) => {
+    return prev + purchase.price * purchase.number;
+  }, 0)
+}
+
+function display() {
+  return purchases.map(purchase => {
+    return `${purchase.price}円:${purchase.number}点\n`;
+  }).join("")
+};
+
+function calcPostageFromPurchase(sum) {
+  if (sum == 0 || sum >= 3000) {
+    return 0
+  } else if (sum < 1000){
+    return 500
+  } else {
+    return 250
+  }
+}
+
+function calc() {
+  const sum = subtotal();
+  const postage = calcPostageFromPurchase(sum);
+  window.alert(`小計は${sum}円、送料は${postage}円です。合計は${sum + postage}円です`);
+  purchases = [];
+  priceElement.value= "";
+  numberElement.value = "";
+}
